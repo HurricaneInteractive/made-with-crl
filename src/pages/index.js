6e265dd85866946b680474eabcacf6bd1a3d19ca
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-11-03 18:24:09 +1100

    Added welcome message

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,13 @@ const IndexPage = ({ data }) => {
       <div className="md:flex">
         <div className="w-full">
           <div className="md:flex flex-wrap -mx-2">
+            <div className="px-2 w-full mb-10">
+              <div className="md:w-1/3">
+                <h2 className="text-2xl mb-3 font-semibold">Welcome to Made with CRL</h2>
+                <p className="text-gray-300">The goal of this project is to highlight libraries created with <a className="underline" href="https://github.com/transitive-bullshit/create-react-library" target="_blank" rel="noopener noreferrer">create-react-library</a>. If you've create a library, feel free to submit a PR on this project.</p>
+                <p className="text-sm text-gray-600">PSST... <span>top corner</span></p>
+              </div>
+            </div>
             {
               libraries && libraries.map(({ node: library }) => (
                 <LibraryCard slug={library.fields.slug} {...library.frontmatter} key={library.id} />
